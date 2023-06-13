7cc05e417e5486488685470a43193402a55f3cf9
Shannorr <theo.pessegue2@gmail.com>
2023-06-13 16:44:51 +0200

    init

---
+++ src/main/java/fr/miage/toulouse/m1/miageland/tpmiageland/TpMiagelandApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m1.miageland.tpmiageland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpMiagelandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpMiagelandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/miage/toulouse/m1/miageland/tpmiageland/TpMiagelandApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m1.miageland.tpmiageland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpMiagelandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
